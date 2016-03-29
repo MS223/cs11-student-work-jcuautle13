c4b3c9d83d2322467fd6029412a050902d9b490a
jcuautle13 <james.cuautle@ms223.org>
2016-03-29 08:44:02 -0400

    Update CountdownV2.py

--- CountdownV2.py
+++ CountdownV2.py
@@ -1,7 +1,7 @@
-x = input("Give me a number")
-while x > 0:
-    print x
-    x = x - 1
-print "BOOM!"
+x = input("Give me a number") # This first starts off with the string, "Give me a number", which then whatever number you put in will be set into the input for x.
+while x > 0: # This can also be seen as a boolean (It's actually a while loop) because if you give the number 10, 10 is greater than 0. 
+    print x # literally prints x, but in this case it's 10. 
+    x = x - 1 # Since we're counting down from 10, it should not have a "+" sign. It needs a "-" sign since the numbers will decrease. 
+print "BOOM!" #Prints Boom! when it hits 0. 
 
 # Add comments next to your code!
