4526bfd3d168b92dbac9be5004b96913ac4de94f
jcuautle13 <james.cuautle@ms223.org>
2016-04-04 08:41:27 -0400

    Update Guessing_Game.py

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,2 +1,12 @@
 username = raw_input("What's your name?").capitalize()
-# Stuck here for some reason.
+upper_bound = input("What's the biggest number you want to guess?")
+guess = input("What's your guess?")
+import random
+random_number = random.randint(0,upper_bound)
+if guess > random:
+    print "Sorry. Your guess is too high!"
+else:
+    print "Sorry. Your guess is too low!"
+while x 
+
+# Almost there. Didn't finish yet. 
