58fbebaf21af0f469d41f355f279a72b69db07a5
jcuautle13 <james.cuautle@ms223.org>
2016-04-11 08:43:05 -0400

    Update April11_DoNow.py

--- April11_DoNow.py
+++ April11_DoNow.py
@@ -1,8 +1,8 @@
 import random
 
 def mystery_function(x, y):
-    random_number = random.randint(0,2) # What's the range of randoms?
-    if random_number > 0: # What's the probability that random_number is greater than 0?
+    random_number = random.randint(0,2) # The range of numbers is 0,1,and 2.
+    if random_number > 0: # There are 3 possible numbers that are larger than 0. 
         z = x + y
     else:
         z = x * y
