b14f8d0cdaf65045ae9cf1fbc725d3c993666f96
jcuautle13 <james.cuautle@ms223.org>
2016-04-18 08:33:40 -0400

    Create practice_makes_perfect.py

---
+++ practice_makes_perfect.py
@@ -0,0 +1,13 @@
+def is_even(x):
+    if (x % 2 == 0):
+        return True
+    else:
+        return False
+is_even(7)
+
+def is_int(x):
+    if is_int == 0:
+        return True
+    else:
+        return False
+is_int(7.5)
