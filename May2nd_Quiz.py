0ea69073e6247ad55dc8e801d93b4d9cb086d3d6
jcuautle13 <james.cuautle@ms223.org>
2016-05-03 09:16:47 -0400

    Update May2nd_Quiz.py

--- May2nd_Quiz.py
+++ May2nd_Quiz.py
@@ -3,6 +3,12 @@ def maxvalue(x):
         print i
 maxvalue(8)
 
-list1 = [1,5,42,23,44]
-list2 = [8,11,69,45,93]
+list1 = [1,18,42,33,44]
+list2 = [8,11,69,12,93]
 def compare_lists(list1,list2):
+    for x in list1:
+        if x > list2[list1.index(x)]:
+            print x
+        else:
+            print list2[list1.index(x)]
+compare_lists(list1,list2)
