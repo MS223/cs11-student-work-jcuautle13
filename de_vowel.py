e9dfa0ea641974f59b76e0d2734fb67d5befd7f5
jcuautle13 <james.cuautle@ms223.org>
2016-05-03 14:03:58 -0400

    Update de_vowel.py

--- de_vowel.py
+++ de_vowel.py
@@ -1,6 +1,6 @@
 vowels = ['a','e','i','o','u']
-string = raw_input("Give me a sentence.")
-
-def de_vowel(string):
-    for x in string:
-        if x in vowels:
+sentence = raw_input("Give me a sentence!").lower() #This will prevent any problems with capitalized sentences.
+for x in sentece:
+    if x in vowels:
+        sentence = sentence.replace(x,'') # This replaces the "x"'s with " '' "
+print sentence # Prints out the sentence without
