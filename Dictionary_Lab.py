40d17b0ae4a1b93e2be494238aa242820fdaaab3
jcuautle13 <james.cuautle@ms223.org>
2016-05-16 08:45:47 -0400

    Create Dictionary_Lab.py

---
+++ Dictionary_Lab.py
@@ -0,0 +1,9 @@
+user_profile = {
+'Name':'James'
+'School': 'The Laboratory School of Finance & Technology'
+'Birthday': 'February 18, 1999'
+'Email': 'James.cuautle@ms223.org'
+'Where are you from?': 'Earth'
+'Number of Siblings': 'I was a mistake that my parents will not make again'
+
+print user_profile[Number of Siblings]
