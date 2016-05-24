93538609f35107df9ee85a88e5770c464d294f51
jcuautle13 <james.cuautle@ms223.org>
2016-05-24 09:30:02 -0400

    Create ColorClass.py

---
+++ ColorClass.py
@@ -0,0 +1,13 @@
+class color(object):
+    """Represents a pet"""
+
+Soul_Black = color()
+Bloody_red = color()
+Money_green = color()
+Metallica_Metallic = color()
+
+
+
+
+def add(my_color1, my_color2, my_color3):
+    
