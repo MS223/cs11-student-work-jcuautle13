67555b3a2db26f4a0a3d857461f2e369efdc8eed
jcuautle13 <james.cuautle@ms223.org>
2016-05-27 08:26:39 -0400

    Create Do Now_May27.py

---
+++ Do Now_May27.py
@@ -0,0 +1,16 @@
+class Pet(object):
+    def __init__(self, name):
+        self.name = name
+
+my_pet = Pet("Nymeria")
+print my_pet.name
+
+#1. I think what __init__ does is that it classifies a certain class that gives it a name. Ex: line 2.
+#2.def __init__(self,name,color):
+#3. def __init__(self,name,color):
+        #self.name = name
+        #self.color = color
+#my pet = Pet("Nymeria")
+#my_pet = Pet("Brown")
+#print my_pet.name
+#print my_pet.color
