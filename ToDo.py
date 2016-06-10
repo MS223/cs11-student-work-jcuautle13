3fe3344e03ed8549dc2c3b8b5e67cba817121965
jcuautle13 <james.cuautle@ms223.org>
2016-06-10 14:43:37 -0400

    Update ToDo.py

--- ToDo.py
+++ ToDo.py
@@ -1,36 +1,34 @@
-action = raw_input("What would you like to do?")
-day = raw_input("What day?").capitalize()
-
-days_of_the_week = {
-    "Monday":[],
-    "Tuesday":[],
-    "Wednesday":[],
-    "Thurday":[],
-    "Friday":[],
-    "Saturday":[],
-    "Sunday":[],
+days_of_week= {
+'Monday': [],
+'Tuesday':[],
+'Wednesday':[],
+'Thursday':[],
+'Friday':[],
+'Saturday':[],
+'Sunday':[],
 }
 
-
-def add():
-    action = ""
-    while action != "nothing":
-        action = raw_input("What are you doing?")
-        if action == "nothing":
-         return None
-        day = raw_input("What day?").capitalize()
-        days_of_the_week[day].append(action)
-        print days_of_the_week
-add()
-
 def get(day):
-    print days_of_the_week[day]
+    print days_of_week[day]
+
+
+def add(action, day):
+    days_of_week[day].append(action)
+
 
 def choice():
-    user_choice = raw_input("How can I help you?")
-    if user_choice == "get":
-        day = raw_input("What day?").capitalize()
-        get(day)
-    if user_choice == "add":
-        add()
+    user_choice= ""
+    action= ""
+    day= ""
+    while user_choice != "exit":
+        user_choice= raw_input("How may I help you?" )
+        if user_choice == "add":
+            action= raw_input("What would you like to do? ")
+            day= raw_input("What day? ").capitalize()
+            add(action,day)
+        elif user_choice == "get":
+            day= raw_input("What day? ").capitalize()
+            get(day)
+
+
 choice()
